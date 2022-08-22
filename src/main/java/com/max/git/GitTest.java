b9c0ec021fc527ee654cd8d17435b227aa26870f
Max-matto <1340898576@qq.com>
2022-08-22 18:06:28 +0800

    frist commit

---
+++ src/main/java/com/max/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.max.git;
+
+/**
+ * @author 麦家宝
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}

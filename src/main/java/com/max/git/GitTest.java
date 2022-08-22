9905eb65a1773b874055b6a26032aab4a2beae7b
Max-matto <1340898576@qq.com>
2022-08-22 18:10:32 +0800

    second commit

--- src/main/java/com/max/git/GitTest.java
+++ src/main/java/com/max/git/GitTest.java
@@ -7,6 +7,6 @@ package com.max.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("hello git2!");
     }
 }
